aae2a5b68752cca3d8f3e72f889a3f20c09f5c33
jrshields86 <jrshields86@gmail.com>
2023-08-31 15:44:41 -0600

    fourth commit

--- app.js
+++ app.js
@@ -6,23 +6,19 @@ const fetchPuppies = async() => {
   const response = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/2307-FTB-ET-WEB-FT/players');
   const json = await response.json();
   puppies = json.data.players;
-  
+  render();
+};
 
-const hash = window.location.hash.slice(1)*1;
-  const html = puppies.map( puppy =>{
-    return `<li><a href='#${puppy.id}' class='${ puppy.id === hash ? 'selected': ''}'>${puppy.name}</a></li>`
-  }).join('');
-  ul.innerHTML = html;
-  
-  
+const render = ()=> {
+    const hash = window.location.hash.slice(1)*1;
+    const html = puppies.map( puppy =>{
+      return `<li><a href='#${puppy.id}' class='${ puppy.id === hash ? 'selected': ''}'>${puppy.name}</a></li>`
+    }).join('');
+    ul.innerHTML = html;
 };
 
 window.addEventListener('hashchange', ()=>{
-    const hash = window.location.hash.slice(1)*1;
-  const html = puppies.map( puppy =>{
-    return `<li><a href='#${puppy.id}' class='${ puppy.id === hash ? 'selected': ''}'>${puppy.name}</a></li>`
-  }).join('');
-  ul.innerHTML = html;
+    render();
 });
 
 fetchPuppies();
