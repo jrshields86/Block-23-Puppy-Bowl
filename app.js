a7fab46d7d9d62161ac3d2bda90327105689c5e3
jrshields86 <jrshields86@gmail.com>
2023-08-31 13:12:33 -0600

    third commit

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const ul = document.querySelector('ul')
-console.log(ul)
+
+let puppies;
 
 const fetchPuppies = async() => {
   const response = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/2307-FTB-ET-WEB-FT/players');
   const json = await response.json();
-  const puppies = json.data.players;
-  console.log(puppies)
+  puppies = json.data.players;
+  
 
+const hash = window.location.hash.slice(1)*1;
   const html = puppies.map( puppy =>{
-    return `<li>${puppy.name}</li>`
+    return `<li><a href='#${puppy.id}' class='${ puppy.id === hash ? 'selected': ''}'>${puppy.name}</a></li>`
   }).join('');
   ul.innerHTML = html;
-  console.log(html)
+  
   
 };
+
+window.addEventListener('hashchange', ()=>{
+    const hash = window.location.hash.slice(1)*1;
+  const html = puppies.map( puppy =>{
+    return `<li><a href='#${puppy.id}' class='${ puppy.id === hash ? 'selected': ''}'>${puppy.name}</a></li>`
+  }).join('');
+  ul.innerHTML = html;
+});
+
 fetchPuppies();
+
